9a471d69f88995ed448d3b2b26b2e8ba4ead2ae8
Your Name <rikin.rathod@matrixhive>
2022-05-24 19:22:25 +0530

    pagination pending

--- task15/datatable.php
+++ task15/datatable.php
@@ -6,81 +6,53 @@ class Makedatatable
     {
         $this->conn = mysqli_connect("localhost", "root", "12345", "country_city_statedetails");
     }
-
-
     public function tableData()
-
     {
-
-    
-
-     $main_query = "SELECT * FROM persondetails";
+        $main_query = "SELECT * FROM persondetails";
         $main_res = mysqli_query($this->conn, $main_query);
+        $output = "";
+        $total_record = mysqli_num_rows($main_res);
+        $limitdata = $_POST['limitdata'];
+        if ($_POST['searchdata']) {
+            $searchdata = $_POST['searchdata'];
 
-        // if (isset($_POST['datas'])) {
-            $limitdata = $_POST['limitdata'];
+            $main_query .= " " . "WHERE" . " " . "fname" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "lname" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "dob" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "age" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "email" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "mobno" . " " . "LIKE" . " " . "'%" . $searchdata . "%' " . "OR" . " " .  "mobno" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "src1" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "camp" . " " . "LIKE" . " " . "'%" . $searchdata . "%'" . "OR" . " " .  "country" . " " . "LIKE" . " " . "'%" . $searchdata . "%'";
+            // echo $main_query;
+        }
+        if ($_POST['limitdata']) {
+            $main_query .= " " . "LIMIT" . " " . $limitdata;
+            // echo $main_query;
 
-            
-            // var_dump($_POST['datas']);
-         
-            if($_POST['searchdata']){
-                $searchdata= $_POST['searchdata'];
+        }
 
-                $main_query .= " ". "WHERE" ." ". "fname". " ". "LIKE"." ". "'%".$searchdata."%'" ."OR". " ".  "lname". " ". "LIKE"." ". "'%".$searchdata."%'" ."OR". " ".  "dob". " ". "LIKE"." ". "'%".$searchdata."%'" ."OR". " ".  "age". " ". "LIKE"." ". "'%".$searchdata."%'"."OR". " ".  "email". " ". "LIKE"." ". "'%".$searchdata."%'"."OR". " ".  "mobno". " ". "LIKE"." ". "'%".$searchdata."%' " ."OR". " ".  "mobno". " ". "LIKE"." ". "'%".$searchdata."%'" ."OR". " ".  "src1". " ". "LIKE"." ". "'%".$searchdata."%'"."OR". " ".  "camp". " ". "LIKE"." ". "'%".$searchdata."%'"."OR". " ".  "country". " ". "LIKE"." ". "'%".$searchdata."%'";
-                // echo $main_query;
+        if ($_POST['page']) {
+            $pages = $_POST['page'];
+            var_dump($pages);
+        }
+        $limit_res = mysqli_query($this->conn, $main_query);
+        if ($limit_res->num_rows > 0) {
+            //        
+            while ($row = $limit_res->fetch_assoc()) {
+?>
+                <tr>
+
+                    <td><input type="checkbox" name="customer_id[]" onclick="datatable()" class="delete_customer" value="<?php echo $row["id"]; ?>" /></td>
+
+                    <td><?php echo $row['fname']; ?></td>
+                    <td><?php echo $row['lname']; ?></td>
+                    <td><?php echo $row['dob']; ?></td>
+                    <td><?php echo $row['age']; ?></td>
+                    <td><?php echo $row['email']; ?></td>
+                    <td><?php echo $row['mobno']; ?></td>
+                    <td><?php echo $row['src1']; ?></td>
+                    <td><?php echo $row['camp']; ?></td>
+                    <td><?php echo $row['country']; ?></td>
+                    <td><a href='index.php?delete="<?php $row['id'] ?> "'>Delete</a></td>
+                </tr>
+<?php
             }
-            if ($_POST['limitdata']) {
-                $main_query .= " " . "LIMIT" . " " . $limitdata;
-                // echo $main_query;
-
-            }
-                $limit_res = mysqli_query($this->conn,$main_query);
-                if ($limit_res->num_rows > 0) {
-                    echo '<table class = "table table-bordered">
-                    <thead class = "bg-primary text-white text-center">
-            <tr>
-            <th>#</th>
-                <th>Id</th>
-                <th>Name</th>
-                <th>Birth Date</th>
-                <th>Age</th>
-                <th>Email id</th>
-                <th>Mobile No</th>
-                <th>Source</th>
-                <th>Campign</th>
-                <th>City</th>
-                <th>Action</th>
-            </tr>
-            <thead>
-            ';
-                    while ($row = $limit_res->fetch_assoc()) {
-
-
-                        echo "<tr>
-                        <td><input type = 'checkbox' onchange ='datatable()' id= ></td>
-
-                        <td>" . $row['fname'] . "</td>
-                        <td>" . $row['lname'] . "</td>
-                        <td>" . $row['dob'] . "</td>
-                        <td>" . $row['age'] . "</td>
-                        <td>" . $row['email'] . "</td>
-                        <td>" . $row['mobno'] . "</td>
-                        <td>" . $row['src1'] . "</td>
-                        <td>" . $row['camp'] . "</td>
-                       <td>" . $row['country'] . "</td>
-                        <td><a href='crud-form.php?delete=" . $row['id'] . "'>Delete</a></td>
-                 </tr>";
-                    }
-                }
-            }
-        
-
-
-
-    // Prepare a select statement
-
-
-
+        }
+    }
 
     public function deleteRecord($id)
     {
@@ -93,14 +65,21 @@ class Makedatatable
             echo "Record does not delete try again";
         }
     }
+    public function multipleDelete($id)
+    {
+
+        echo "deleteRecord";
+        $query = mysqli_query($this->conn, "DELETE FROM persondetails WHERE id = '$id'");
+        if ($query == true) {
+            header("Location:view_empdetails.php");
+        } else {
+            echo "Record does not delete try again";
+        }
+    }
 }
-
-
-
-
 
 $rowobj = new Makedatatable();
-if(count($_POST) > 0){
+if (count($_POST) > 0) {
+
     $rowobj->tableData();
 }
-
--- task15/index.php
+++ task15/index.php
@@ -1,29 +1,21 @@
 <?php
-
-
 include 'datatable.php';
-
-
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $deleteId = $_GET['id'];
-    $rowobj->deleteRecord($deleteId);
+    $rowobj->deleteRecord($id);
 }
-
-
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https:cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https:cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+    <script src="https:ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="http:ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>See Employee details</title>
@@ -36,6 +28,27 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
     .emailserror {
         color: red;
 
+    }
+
+    .pagination {
+        margin-left: 80%;
+    }
+
+    .pagination a {
+        color: black;
+        float: left;
+        padding: 8px 16px;
+        text-decoration: none;
+        transition: background-color .3s;
+    }
+
+    .pagination a.active {
+        background-color: dodgerblue;
+        color: white;
+    }
+
+    .pagination a:hover:not(.active) {
+        background-color: #ddd;
     }
 </style>
 
@@ -58,58 +71,85 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
             </select>
 
             <div class="livesearch">
-                <input type="text" placeholder="Search" onchange="datatable()" class="form-control" id="searchs" style="width: 30%; margin-left:70%;    margin-top: -36px;
+                <input type="text" placeholder="Search" onkeyup="datatable()" class="form-control" id="searchs" style="width: 30%; margin-left:70%;    margin-top: -36px;
 ">
             </div>
             <br>
+
+            <table class="table table-bordered">
+                <thead class="bg-primary text-white text-center">
+                    <tr>
+                        <th>#</th>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Birth Date</th>
+                        <th>Age</th>
+                        <th>Email id</th>
+                        <th>Mobile No</th>
+                        <th>Source</th>
+                        <th>Campign</th>
+                        <th>City</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody id="response" class="pag_data">
+
+                </tbody>
+            </table>
+            <div id="pagination">
+                <a class="active" id="1" href="">1</a>
+                <a id="2" href="">2</a>
+                <a id="3" href="">3</a>
+            </div>
             <!-- <div class="deletebutton">
-            <button type="button" class="btn btn-success btn-sm delete"  data-id="<?=$row['id'];?>">Delete</button>            </div> -->
+            <button type="button" class="btn btn-success btn-sm delete"  data-id="<?= $row['id']; ?>">Delete</button>            </div> -->
 
 
             <script>
-                function datatable() {
+                function datatable(page) {
                     var limitdata = $("#limit").val();
                     var searchdata = $("#searchs").val();
+                    if (searchdata <= 3) {
+                        $("#err")
+                    }
 
-
-                    console.log(limitdata)
-                    console.log(searchdata)                    
-
-
-
-
+                    // var pagination= $("button").text();
+                    // console.log(pagination);
 
                     $.ajax({
                         type: "POST",
                         url: "datatable.php",
                         data: {
                             limitdata: limitdata,
-                            searchdata:searchdata
+                            searchdata: searchdata,
+                            data: {
+                                page: page
+                            }
 
                         },
 
                         success: function(datatables) {
 
-
-                            $("table").replaceWith(datatables);
+                            $("#response").html(datatables);
 
 
                         }
-
-
                     });
-
-
-
-
-
-                  
- }
+                }
 
                 $(document).ready(function() {
-                   datatable();
+                    datatable();
 
                 });
+
+                $(document).on("click", "#pagination a", function(e) {
+                    //alert("a");
+                    e.preventDefault();
+                    var page_id = $(this).attr("id");
+                    console.log(page_id);
+                    datatable(page_id);
+
+                })
             </script>
         </div>
 
